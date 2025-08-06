c2c21dc603dadc6b9766fda3e1e24bb5a50f1026
biamouras <biamouras@gmail.com>
2025-08-06 18:22:06 -0300

    label date tags

--- R/import.R
+++ R/import.R
@@ -11,3 +11,37 @@ import_day_file <- function() {
 import_hour_file <- function() {
   vroom::vroom("data-raw/hour.csv")
 }
+
+label_date_tags <- function(tab){
+  tab |>
+    dplyr::mutate(
+      season = factor(
+        season,
+        levels = 1:4,
+        labels = c("Spring", "Summer", "Fall", "Winter")
+      ),
+      weekday = factor(
+        weekday,
+        levels = 0:6,
+        labels = lubridate::wday(1:7, label = TRUE)
+      ),
+      holiday = factor(
+        holiday,
+        labels = c("No", "Yes")
+      ),
+      workingday = factor(
+        workingday,
+        labels = c("Working day", "Weekend or Holiday")
+      ),
+      weathersit = factor(
+        weathersit,
+        levels = 1:4,
+        labels = c(
+          "Clear",
+          "Misty",
+          "Light rain or snow",
+          "Bad weather"
+        )
+      )
+    )
+}
--- _targets.R
+++ _targets.R
@@ -4,6 +4,8 @@ tar_source()
 
 list(
   tar_target(download, download_zip()),
-  tar_target(tab_day, import_day_file()),
-  tar_target(tab_hour, import_hour_file())
+  tar_target(import_day, import_day_file()),
+  tar_target(import_hour, import_hour_file()),
+  tar_target(tab_day, label_date_tags(import_day)),
+  tar_target(tab_hour, label_date_tags(import_hour))
 )
